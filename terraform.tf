bb9152e1f5bf572657961b969a0d888fab6844bd
androidbasico2016 <wamanqui@gmail.com>
2025-11-03 18:01:04 -0500

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.92"
     }
+
+    singlestoredb = {
+      source = "singlestore-labs/singlestoredb"
+      version = "0.1.2"
+    }
   }
 
   required_version = ">= 1.2"
+
+
+
 }
